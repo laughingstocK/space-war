9cb4ac2a2dec9205d51c0a42b960090baa358e44
laughingstocK <kerkieat@gmail.com>
2018-05-15 00:33:14 +0700

    แก้ 2 player

--- f2/spw/GameEngine.java
+++ f2/spw/GameEngine.java
@@ -11,147 +11,150 @@ import java.util.Iterator;
 
 import javax.swing.Timer;
 
-
-public class GameEngine implements KeyListener, GameReporter{
+public class GameEngine implements KeyListener, GameReporter {
 	GamePanel gp;
-		
+
 	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();
 	private ArrayList<Coin> coins = new ArrayList<Coin>();
 	private SpaceShip v;
-	private SpaceShip v2;	
+	private SpaceShip v2;
 	private int x;
 	private Timer timer;
 	private int a = 0;
 	private long score = 0;
 	private long score2 = 0;
 	private double difficulty = 0.1;
-	
+
 	public GameEngine(GamePanel gp, SpaceShip v, SpaceShip v2) {
 		this.gp = gp;
 		this.v = v;
-		this.v2 = v2;		
-		
+		this.v2 = v2;
+
 		gp.sprites.add(v);
 		gp.sprites.add(v2);
-		
+
 		timer = new Timer(50, new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				process();
 			}
 		});
 		timer.setRepeats(true);
-		
+
 	}
 
-	
-	public void start(){
+	public void start() {
 		timer.start();
 	}
-	
-	private void generateEnemy(){
-		Enemy e = new Enemy((int)(Math.random()*390), 30);
+
+	private void generateEnemy() {
+		Enemy e = new Enemy((int) (Math.random() * 390), 30);
 		gp.sprites.add(e);
 		enemies.add(e);
 	}
 
-	private void generateCoin(){
-		Coin c = new Coin((int)(Math.random()*390), 30);
+	private void generateCoin() {
+		Coin c = new Coin((int) (Math.random() * 390), 30);
 		gp.sprites.add(c);
 		coins.add(c);
-		score += 100;
+		//score += 100; for?
 	}
 
-	
-	private void process(){
-		if(Math.random() < difficulty){
+	private void process() {
+		if (Math.random() < difficulty) {
 			generateEnemy();
 		}
-		
-		if(getScore()%1000 == 0 && getScore() != 0 && a == 0){
+
+		if (((getScore() % 1000 == 0 && getScore() != 0) || (getScore2() % 1000 == 0 && getScore2() != 0) )&& a == 0) {
 			generateCoin();
-			a++;		
+			a++;
 		}
 
 		Iterator<Enemy> e_iter = enemies.iterator();
 		Iterator<Coin> c_iter = coins.iterator();
-		try{
-			while(e_iter.hasNext()){
+		try {
+			while (e_iter.hasNext()) {
 				Enemy e = e_iter.next();
 
 				e.proceed();
 
-				if(!e.isAlive()){
+				if (!e.isAlive()) {
 					e_iter.remove();
 					gp.sprites.remove(e);
-					if(v.isAlive){
+					if (v.isAlive) {
 						score += 100;
 					}
-						
-					score2 += 100;
-				}
-	
-				while(c_iter.hasNext()){
+					if (v2.isAlive) {
+						score2 += 100;
+					}
 
-				Coin c = c_iter.next();
-				c.proceedSpeed();
-
-				if(!c.isAlive()){
-					c_iter.remove();
-					gp.sprites.remove(c);
-					score += 0;
-					score2 += 0;
-					a=0;
 				}
+
+				while (c_iter.hasNext()) {
+
+					Coin c = c_iter.next();
+					c.proceedSpeed();
+
+					if (!c.isAlive()) {
+						c_iter.remove();
+						gp.sprites.remove(c);
+						score += 0;
+						score2 += 0;
+						a = 0;
+					}
 				}
 			}
-			if(getScore() > 5000){
-				difficulty+= 0.1;
+			if (getScore() > 5000 || getScore2() > 5000 ) {
+				difficulty += 0.1;
 			}
-		}catch(Exception e){
+		} catch (Exception e) {
 
 		}
-		
-		
+
 		gp.updateGameUI(this);
-		
+
 		Rectangle2D.Double vr = v.getRectangle();
 		Rectangle2D.Double vr2 = v2.getRectangle();
 		Rectangle2D.Double er;
 		Rectangle2D.Double cr;
 
-		for(Enemy e : enemies){
+		for (Enemy e : enemies) {
 			er = e.getRectangle();
-			if(er.intersects(vr)){
+			if (er.intersects(vr)) {
 				gp.sprites.remove(v);
 				v.isAlive = false;
+				if (v2.isAlive == false && v.isAlive == false)
+					die();
 				return;
 			}
-			if(er.intersects(vr2)){
-				die();
+			if (er.intersects(vr2)) {
+				v2.isAlive = false;
+				gp.sprites.remove(v2);
+				if (v2.isAlive == false && v.isAlive == false)
+					die();
 				return;
 			}
 		}
 
-		for(Coin c : coins){
+		for (Coin c : coins) {
 			cr = c.getRectangle();
-			if(cr.intersects(vr)){
+			if (cr.intersects(vr)) {
 				score += 300;
 				return;
 			}
 
-			if(cr.intersects(vr2)){
+			if (cr.intersects(vr2)) {
 				score2 += 300;
 				return;
 			}
 		}
 	}
-	
-	public void die(){
+
+	public void die() {
 		timer.stop();
 	}
-	
+
 	void controlVehicle(KeyEvent e) {
 		switch (e.getKeyCode()) {
 		case KeyEvent.VK_LEFT:
@@ -172,27 +175,27 @@ public class GameEngine implements KeyListener, GameReporter{
 		}
 	}
 
-	public long getScore(){
+	public long getScore() {
 		return score;
 	}
 
-	public long getScore2(){
+	public long getScore2() {
 		return score2;
 	}
-	
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		controlVehicle(e);
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		//do nothing
+		// do nothing
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		//do nothing		
+		// do nothing
 	}
 }
